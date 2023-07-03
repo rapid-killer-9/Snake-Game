a6179747c0d45bdcf8064e0ea350fcc33734ee81
Harsh Jha <hjha0695@gmail.com>
2023-07-03 09:54:04 +0530

    Function Template created for js

---
+++ main.js
@@ -0,0 +1,79 @@
+const gameBoard = document.querySelector('#gameBoard');
+const ctx = gameBoard.getContext('2d');
+const scoreText = document.querySelector('#scoreText');
+const resetBtn = document.querySelector('#resetBtn');
+
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = '#000000';
+const snakeColor = '#ffffff';
+const snakeBorder = '#000000';
+const foodColor = '#ff0000';
+
+const unitSize = 25;
+let running = false;
+
+let xVelocity = unitSize;
+let yVelocity = 0;
+
+let foodX;
+let foodY;
+
+let score = 0;
+
+let snake = [
+   {x: unitSize*4, y:0},
+    {x: unitSize*3, y:0},
+    {x: unitSize*2, y:0},
+    {x: unitSize, y:0},
+    {x: 0, y:0}
+];
+
+window.addEventListener('keydown',changeDirection);
+resetBtn.addEventListener('click',resetGame);
+
+gameStart();
+
+function gameStart(){
+
+};
+
+function nextTick(){
+
+};
+
+function clearBoard(){
+
+};
+
+function createFood(){
+
+};
+
+function drawFood(){
+    
+};
+
+function moveSnake(){
+
+};
+
+function drawSnake(){
+    
+};
+
+function changeDirection(){
+
+};
+
+function checkGameOver(){
+
+};
+
+function displayGameOver(){
+
+};
+
+function resetGame(){
+        
+};
